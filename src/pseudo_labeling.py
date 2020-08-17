4446f478758ecb9afcea63f919bd38b2974884da
chizuchizu <ochimasumi@gmail.com>
2020-08-17 21:16:43 +0900

    pseudo

---
+++ src/pseudo_labeling.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+test = pd.read_csv("../data/test.csv")
+train = pd.read_csv("../data/train.csv")
+test_pred = pd.read_csv("../data/languages/test_default.csv").iloc[:, 1:]
+
+"""
+CEが2.5以上のものをを利用
+"""
+
+test["jobflag"] = test_pred["default_pred"]
+
+test = test[test_pred.iloc[:, 1:].max(axis=1) > 2.5]
+
+train = pd.concat([train, test])
+
+train.to_csv("../data/pseudo_train.csv", index=False)
