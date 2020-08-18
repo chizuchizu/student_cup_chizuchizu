57560c2dfd92806d7e6fa494dda85a65a3157ac3
chizuchizu <ochimasumi@gmail.com>
2020-08-18 13:34:25 +0900

    testの出力ができるようになった

--- src/lstm_chizuchizu.py
+++ src/lstm_chizuchizu.py
@@ -26,7 +26,7 @@ TEXT_COL = "description"
 TARGET = "jobflag"
 NUM_CLASS = 4
 N_FOLDS = 4
-BS = 64
+BS = 256
 NUM_EPOCHS = 50
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -78,6 +78,12 @@ train_ds, test_ds = torchtext.data.TabularDataset.splits(
     fields=[('text', TEXT), ('label', LABEL)]
 )
 
+test_ds = torchtext.data.TabularDataset(
+    path=BASE_PATH + "test_x.csv",
+    format="csv",
+    fields=[("text", TEXT)],
+)
+
 # traindataをvalとtrainに分割(あとでcvをじっそうしたい)
 train_ds, val_ds = train_ds.split(split_ratio=0.8, random_state=random.seed(SEED))
 # print(vars(train_ds[0]))
@@ -90,6 +96,7 @@ else:
     fasttext = Vectors(name='.vector_cache/wiki.en.vec')
 
 TEXT.build_vocab(train_ds, vectors=fasttext, min_freq=3)  # buildしないといけないらしいよくわからない
+TEXT.build_vocab(test_ds, vectors=fasttext, min_freq=3)
 
 # dataloaderの作成(cv実装したい)
 train_dl = torchtext.data.Iterator(train_ds, batch_size=BS, train=True)
@@ -105,29 +112,46 @@ embeddingsはidでくるものをvectorにする。
 class LSTMClassifier(nn.Module):
     def __init__(self, text_id, hidden_dim, num_label):
         super(LSTMClassifier, self).__init__()
+        self.gru_hidden_size = 64
+        self.lstm_hidden_size = 300
         self.embeddings = nn.Embedding.from_pretrained(
             embeddings=text_id, freeze=True
         )
         self.embedding_dropout = nn.Dropout2d(0.2)
-        self.lstm = nn.LSTM(300, hidden_dim, batch_first=True)
+        self.lstm = nn.LSTM(self.lstm_hidden_size, hidden_dim, batch_first=True, bidirectional=True)
 
-        self.gru = nn.GRU(256, 64, bidirectional=True, batch_first=True)
-
+        self.gru = nn.GRU(hidden_dim * 2, self.gru_hidden_size, bidirectional=True, batch_first=True)
+        self.linear = nn.Linear(self.gru_hidden_size * 6, 20)
         self.cls = nn.Linear(hidden_dim, num_label)
+
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.1)
+        self.out = nn.Linear(self.gru_hidden_size * 6, num_label)
         # self.softmax = nn.LogSoftmax()
+
+    def apply_spatial_dropout(self, h_embedding):
+        h_embedding = h_embedding.transpose(1, 2).unsqueeze(2)
+        h_embedding = self.embedding_dropout(h_embedding).squeeze(2).transpose(1, 2)
+        return h_embedding
 
     def forward(self, x):
         x_vec = self.embeddings(x)
+        x_vec = self.apply_spatial_dropout(x_vec)
+
         h_lstm, lstm_out = self.lstm(x_vec)
         h_gru, hh_gru = self.gru(h_lstm)
-        hh_gru = hh_gru.view(-1, 64*2)
+        hh_gru = hh_gru.view(-1, self.gru_hidden_size * 2)
 
         avg_pool = torch.mean(h_gru, 1)
-        # max_pool, _ = torch.max(h_gru, 1)
-        conc = torch.cat((hh_gru, avg_pool), 1)
+        max_pool, _ = torch.max(h_gru, 1)
+        conc = torch.cat((hh_gru, avg_pool, max_pool), 1)
 
-        # out = self.cls(lstm_out[0].squeeze())
-        out = self.cls(conc)
+        # conc = self.linear(conc)
+
+        # conc = self.relu(conc)
+        conc = self.dropout(conc)
+
+        out = self.out(conc)
         return out
 
 
@@ -183,17 +207,20 @@ def scale_cos(start, end, x):
     return start + (1 + np.cos(np.pi * (1 - x))) * (end - start) / 2
 
 
-def eval_model(model, data_loader):
+def eval_model(model, data_loader, is_train=False):
     all_labels = []
     all_preds = []
     for batch in data_loader:
         inputs = batch.text.to(device)
-        labels = batch.label.to(device)
+        if is_train:
+            labels = batch.label.to(device)
         with torch.set_grad_enabled(True):
             outputs = model(inputs)
             _, pred = torch.max(outputs, 1)
-            all_labels += labels.tolist()
+            if is_train:
+                all_labels += labels.tolist()
             all_preds += pred.tolist()
+
     return all_labels, all_preds
 
 
@@ -222,23 +249,12 @@ def train_model(model, dl_dict, criterion, optimizer, num_epochs):
                 optimizer.step()
                 all_loss += loss.item()
         print("train | epoch", epoch + 1, " | ", "loss", all_loss / len(dl_dict["train"]))
-        all_labels, all_preds = eval_model(model, dl_dict["val"])
-        # for batch in (dl_dict['val']):
-        #     inputs = batch.text.to(device)  # 文章
-        #     labels = batch.label.to(device)
-        #     optimizer.zero_grad()
-        #     with torch.set_grad_enabled(False):
-        #         outputs = model(inputs)
-        #         loss = criterion(outputs, labels)
-        #         _, preds = torch.max(outputs, 1)
-        #         all_labels += labels.tolist()
-        #         all_preds += preds.tolist()
-        # print(loss)
-        # print(all_preds)
+        all_labels, all_preds = eval_model(model, dl_dict["val"], is_train=True)
+
         train_f1 = f1_score(all_labels, all_preds, average="macro")
         print("val | epoch", epoch + 1, " | ", "f1", train_f1)
 
-        # all_test_preds.append(eval_model(model, dl_dict["test"])[1])
+        all_test_preds.append(eval_model(model, dl_dict["test"], is_train=False)[1])
         # for batch in dl_dict["test"]:
         #     inputs = batch.text.to(device)  # 文章
         #     labels = batch.label.to(device)
@@ -254,11 +270,12 @@ def train_model(model, dl_dict, criterion, optimizer, num_epochs):
     checkpoint_weights = np.array([2 ** epoch for epoch in range(num_epochs)])
     checkpoint_weights = checkpoint_weights / checkpoint_weights.sum()
 
-    # test_y = np.average(all_test_preds, weights=checkpoint_weights, axis=0)
+    test_y = np.average(all_test_preds, weights=checkpoint_weights, axis=0)
+    test_y = np.round(test_y).astype(int)
 
     # test_y = np.mean([])
 
-    return model
+    return model, test_y
 
-
-train_model(model, dl_dict, criterion, optimizer, NUM_EPOCHS)
+model, test_y = train_model(model, dl_dict, criterion, optimizer, NUM_EPOCHS)
+print("DONE")
