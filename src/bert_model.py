064a07e40798b8f90c23320b1a98740a6017f58d
chizuchizu <ochimasumi@gmail.com>
2020-08-18 13:55:08 +0900

    pseudo labelingのbaseline

--- src/bert_model.py
+++ src/bert_model.py
@@ -12,7 +12,7 @@ import numpy as np
 import pulp
 
 from sklearn.metrics import f1_score
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, KFold
 from sklearn.preprocessing import MinMaxScaler
 
 from scipy import stats
@@ -76,6 +76,69 @@ def hack(prob, hack=True):
     else:
         return prob.argmax(axis=1)
 
+
+def all_train(train, test, params, model_name, model_type, lb_hack):
+    weight = len(train) / train["label"].value_counts().sort_index().values
+
+    model = ClassificationModel(model_type=model_type, model_name=model_name, num_labels=4,
+                                args=params, use_cuda=True, weight=weight.tolist())
+    model.train_model(train)
+
+    pred, raw_outputs = model.predict(test["description"])
+
+    y_pred = hack(pred, lb_hack)
+    return y_pred
+
+
+def cross_pseudo_labeling(train, test, params, n_folds, model_name, model_type, lb_hack):
+    splits = list(
+        StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=1234).split(train["text"], train["label"])
+    )
+    splits_test = list(
+        KFold(n_splits=n_folds, shuffle=True, random_state=1234).split(test["description"])
+    )
+
+    y_pred = np.zeros((test.shape[0], 4))
+    oof = np.zeros(train.shape[0])
+    oof_raw = np.zeros((train.shape[0], 4))
+    weight = len(train) / train["label"].value_counts().sort_index().values
+
+    f1_score = 0
+
+    for fold, ((train_idx, valid_idx), (train_test_idx, test_idx)) in enumerate(zip(splits, splits_test)):
+        X_train = pd.concat([train.iloc[train_idx], test.iloc[train_test_idx]])
+        X_valid = train.iloc[valid_idx]
+        model = ClassificationModel(model_type=model_type, model_name=model_name, num_labels=4,
+                                    args=params, use_cuda=True, weight=weight.tolist())
+
+        model.train_model(X_train)
+
+        result, model_outputs, wrong_predictions = model.eval_model(X_valid, f1=metric_f1)
+        print(result)
+        f1_score += result["f1"] / n_folds
+
+        fold_pred, raw_outputs = model.predict(test['description'])
+        # y_pred[:, fold] = hack(raw_outputs)
+        y_pred[test_idx, :] = raw_outputs
+
+        oof_pred, oof_outputs = model.predict(X_valid["text"].values)  # 謎のバグが発生するので
+        oof[valid_idx] = oof_pred
+        oof_raw[valid_idx, :] = oof_outputs
+        # oof[valid_idx] = hack(oof_outputs)
+
+    print(f"mean f1_score: {f1_score}")
+
+    raw_pred = y_pred.copy()
+    y_pred = hack(y_pred, lb_hack)
+
+    # oof = hack(oof_raw)
+
+    # y_pred = stats.mode(y_pred, axis=1)[0].flatten().astype(int)
+
+    test_pred = pd.DataFrame(np.concatenate([y_pred.reshape(-1, 1), raw_pred], 1))
+    oof_pred = pd.DataFrame(np.concatenate([oof.reshape(-1, 1), oof_raw], 1))
+
+    return test_pred, f1_score, oof_pred
 
 def model(train, test, params, n_folds, model_name, model_type, lb_hack):
     kfold = StratifiedKFold(n_splits=n_folds)
---
+++ src/pseudo_bert.py
@@ -0,0 +1,86 @@
+"""
+https://www.kaggle.com/sakami/single-lstm-3rd-place
+
+1. train全データ学習でtestを予測
+2. テストデータでKfoldしリークしないようにtestを再予測
+
+今の所trainもkfoldしている（stacking用）が、その必要は果たして必要なのか……　私にはわからない。
+"""
+
+import pandas as pd
+from src import bert_model
+
+LANGUAGES = ["ja", "de", "fr", "default"]
+
+DATA_PATH = "../data/"
+SEED = 2020
+N_FOLDS = 4
+MODEL_TYPE = "bert"
+MODEL_NAME = "bert-base-uncased"
+# MODEL_TYPE = "xlnet"
+# MODEL_NAME = "xlnet-base-cased"  # xxlarge-v2
+LB_HACK = False
+params = {
+    # "output_dir": "outputs/",
+    "max_seq_length": 64,
+    "train_batch_size": 32,
+    "eval_batch_size": 64,
+    "num_train_epochs": 5,
+    "learning_rate": 1e-4,
+    "reprocess_input_data": True,
+    # "do_lower_case": True,
+    "manual_seed": SEED,
+    "verbose": False,
+    "save_model_every_epoch": False,
+    "save_eval_checkpoints": False,
+    "overwrite_output_dir": True,
+}
+
+bert_model.seed_everything(SEED)
+
+test = pd.read_csv(f"{DATA_PATH}test.csv").drop(['id'], axis=1)
+
+
+def preprocess(data, augment=True):
+    if augment:
+        data = data.drop(columns=['id', "description"])
+        data = data.rename(columns={"transrated": 'text', "jobflag": 'label'})
+    else:
+        data = data.drop(columns=["id"])
+        data = data.rename(columns={"description": "text", "jobflag": "label"})
+    # data = data.rename(columns={"description": "text", "jobflag": "label"})
+    data['label'] -= 1
+    # 順序が違うとだめらしいので
+    data = data.reindex(columns=["text", "label"])
+
+    return data
+
+
+"""全データ学習"""
+
+columns = ["pred", "1_ce", "2_ce", "3_ce", "4_ce"]
+for language in LANGUAGES:
+    print(language)
+    if language != "default":
+        train = preprocess(pd.read_csv(f"{DATA_PATH}{DATA_PATH}train_{language}_en.csv"))
+    else:
+        train = preprocess(pd.read_csv(f"{DATA_PATH}train.csv"), augment=False)
+    """pseudo labeling"""
+
+    print("all_data")
+    test_pred = bert_model.all_train(train, test, params, MODEL_NAME, MODEL_TYPE, LB_HACK)
+    # test_pred.to_csv(f"{DATA_PATH}languages/test_{language}_{MODEL_NAME}_{LB_HACK}.csv"
+    test["jobflag"] = test_pred.copy()
+
+    print("pseudo labeling")
+    test_pred, f1_score, oof_pred = bert_model.cross_pseudo_labeling(train, test, params, N_FOLDS, MODEL_NAME,
+                                                                     MODEL_TYPE, LB_HACK)
+    print(language, "model training have done")
+
+    lang_columns = [language + "_" + x for x in columns]
+
+    test_pred.columns = lang_columns
+    oof_pred.columns = lang_columns
+
+    test_pred.to_csv(f"{DATA_PATH}languages/test_{language}_{MODEL_NAME}_{LB_HACK}_pseudo.csv")
+    oof_pred.to_csv(f"{DATA_PATH}languages/train_{language}_{MODEL_NAME}_{LB_HACK}_pseudo.csv")
