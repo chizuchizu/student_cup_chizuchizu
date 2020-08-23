bca379974fbb37ec3207b910ae7d93966984ad19
chizuchizu <ochimasumi@gmail.com>
2020-08-23 13:27:20 +0900

    transformers用のpreprocess完成

---
+++ preprocess/preprocess_transformers.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+DATA_PATH = "../for_train_data/"
+
+
+def preprocess(data, augment):
+    if augment:
+        data = data.drop(columns=['id', "description"])
+        data = data.rename(columns={"transrated": 'text', "jobflag": 'label'})
+    else:
+        data = data.drop(columns=["id"])
+        data = data.rename(columns={"description": "text", "jobflag": "label"})
+    # data = data.rename(columns={"description": "text", "jobflag": "label"})
+    data['label'] -= 1
+    # 順序が違うとだめらしいので
+    data = data.reindex(columns=["text", "label"])
+
+    return data
+
+
+LANGUAGES = ["ja", "de", "fr", "default"]
+for language in LANGUAGES:
+    print(language)
+    if language != "default":
+        train = preprocess(pd.read_csv(f"{DATA_PATH}{DATA_PATH}train_{language}_en.csv"), augment=True)
+    else:
+        train = preprocess(pd.read_csv(f"{DATA_PATH}train.csv"), augment=False)
+
+    train.to_csv(f"../for_train_data/train_{language}_base.csv", index=False)
